5a98da50cef3265826fbd67effcfc9824c6085b3
Merkushev Egor <89021645+emgeorrk@users.noreply.github.com>
2023-03-18 02:18:26 +0300

    Update bfs_maze.py

--- practicum_2/homework/bfs_maze.py
+++ practicum_2/homework/bfs_maze.py
@@ -1,5 +1,6 @@
 from time import perf_counter
-
+import queue
+import networkx as nx
 
 class Maze:
     def __init__(self, list_view: list[list[str]]) -> None:
@@ -39,9 +40,41 @@ class Maze:
 def solve(maze: Maze) -> None:
     path = ""  # solution as a string made of "L", "R", "U", "D"
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    G = nx.Graph()
+
+    for i in range(0, len(maze.list_view)):
+        for j in range(0, len(maze.list_view[i])):
+            if maze.list_view[i][j] == '#': continue
+            if maze.list_view[i][j] == 'X': finish = (i,j)
+            if i+1 < len(maze.list_view) and maze.list_view[i+1][j] != '#': G.add_edge((i,j), (i+1,j))
+            if j+1 < len(maze.list_view[i]) and maze.list_view[i][j+1] != '#': G.add_edge((i,j), (i,j+1))
+
+    dist = {}
+    dist[0,maze.start_j] = 0
+
+    q = queue.Queue()
+    q.put((0,maze.start_j))
+
+    while not q.empty():
+        v = q.get()
+        for u in G.neighbors(v):
+            if dist.get(u) == None or dist[u] > dist[v] + 1:
+                dist[u] = dist[v] + 1
+                q.put(u)
+
+    while finish != (0, maze.start_j):
+        print(finish)
+        for u in G.neighbors(finish):
+            u_i,u_j = u
+            finish_i, finish_j = finish
+            if dist[u] == dist[finish] - 1:
+                if finish_i - 1 == u_i and finish_j == u_j: path += 'D'
+                if finish_i + 1 == u_i and finish_j == u_j: path += 'U'
+                if finish_i == u_i and finish_j - 1 == u_j: path += 'R'
+                if finish_i == u_i and finish_j + 1 == u_j: path += 'L'
+                finish = u
+                break
+    path = reversed(path)
 
     print(f"Found: {path}")
     maze.print(path)
