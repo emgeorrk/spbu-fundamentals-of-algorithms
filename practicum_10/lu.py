e098a3ddef48466f63453d0e08535fb8acaf7e3e
Merkushev Egor <nounstep@gmail.com>
2023-05-20 16:06:06 +0300

    solved 10

--- practicum_10/lu.py
+++ practicum_10/lu.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.typing import NDArray
+from typing import List
 from math import fabs
 
 
@@ -8,9 +9,6 @@ def lu(A: NDArray, permute: bool) -> tuple[NDArray, NDArray, NDArray]:
         n = len(A)
         L = np.zeros_like(A)
         U = A.copy()
-        for i in range(n):
-            for j in range(n):
-                L[i][j] = 0.0
 
         for i in range(n):
             for j in range(i, n):
@@ -28,24 +26,22 @@ def lu(A: NDArray, permute: bool) -> tuple[NDArray, NDArray, NDArray]:
     else:
         n = len(A)
         C = A
-        P = np.zeros_like(A)
-        for i in range(n): P[i][i] = 1.
+        P = np.eye(n)
 
         for i in range(n):
-            pivotValue = 0.
+            pivotValue = 0
             pivot = -1.
             for row in range(i, n):
-                if fabs(C[row][i] > pivotValue):
+                if fabs(C[row][i]) > pivotValue:
                     pivotValue = fabs(C[row][i])
                     pivot = row
             if pivotValue != 0:
-                P[pivot], P[i] = P[i], P[pivot]
-                C[pivot], C[i] = C[i], C[pivot]
+                P[[pivot, i]] = P[[i, pivot]]
+                C[[pivot, i]] = C[[i, pivot]]
                 for j in range(i+1, n):
                     C[j][i] /= C[i][i]
                     for k in range(i+1, n):
                         C[j][k] -= C[j][i]*C[i][k]
-        print(C)
         L = np.zeros_like(A)
         U = np.zeros_like(A)
         for i in range(n):
@@ -58,24 +54,42 @@ def lu(A: NDArray, permute: bool) -> tuple[NDArray, NDArray, NDArray]:
         return (L, U, P)
 
 
-def solve(L: NDArray, U: NDArray, P: NDArray, b: NDArray) -> NDArray:
-    Y = np.linalg.solve(L, P @ b)
-    X = np.linalg.solve(U, Y)
+def forward_substitution(L, b):
+    n = len(b)
+    y = np.zeros(n)
+
+    for i in range(n):
+        y[i] = b[i]
+        for j in range(i):
+            y[i] -= L[i, j] * y[j]
+        y[i] /= L[i, i]
+
+    return y
+
+def backward_substitution(U, b):
+    n = len(b)
+    x = np.zeros(n)
+
+    for i in range(n - 1, -1, -1):
+        x[i] = b[i]
+        for j in range(i + 1, n):
+            x[i] -= U[i, j] * x[j]
+        x[i] /= U[i, i]
+
+    return x
+
+def solve(L, U, P, b):
+    Pb = P.dot(b)
+    Y = forward_substitution(L, Pb)
+    X = backward_substitution(U, Y)
 
     return X
-
-
-def get_A_b(a_11: float, b_1: float) -> tuple[NDArray, NDArray]:
-    A = np.array([[a_11, -7.0, 0.0], [-3.0, 6.0, 2.0], [5.0, -1.0, 5.0]])
-    b = np.array([b_1, 12.0, -39.0])
-    return A, b
 
 
 def get_A_b(a_11: float, b_1: float) -> tuple[NDArray, NDArray]:
     A = np.array([[a_11, 1.0, -3.0], [6.0, 2.0, 5.0], [1.0, 4.0, -3.0]])
     b = np.array([b_1, 12.0, -39.0])
     return A, b
-
 
 if __name__ == "__main__":
     # Let's implement the LU decomposition with and without pivoting
@@ -85,9 +99,9 @@ if __name__ == "__main__":
     b_1 = -16 + 10 ** (-p)  # add/remove 10**(-p) to check instability
     A, b = get_A_b(a_11, b_1)
     # With pivoting
-    # L, U, P = lu(A, permute=True)
-    # x = solve(L, U, P, b)
-    # assert np.all(np.isclose(x, [1, -7, 4])), f"The anwser {x} is not accurate enough"
+    L, U, P = lu(A, permute=True)
+    x = solve(L, U, P, b)
+    assert np.all(np.isclose(x, [1, -7, 4])), f"The anwser {x} is not accurate enough"
     # Without pivoting
     L, U, P = lu(A, permute=False)
     x_ = solve(L, U, P, b)
